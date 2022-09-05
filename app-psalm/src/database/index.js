aa2f32c49f64f8ea67c3c087fcdcb1e018ec8557
Gabriel Ribas <gabriel.ribass@gmail.com>
2022-09-05 17:12:47 -0300

    Update index.js

--- app-psalm/src/database/index.js
+++ app-psalm/src/database/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 //mongoose.connect('mongodb://localhost/salmos', { useMongoClient: true } );
+//Here i need changing to get from ENV.
+// Eg.: require('dotenv').config(); process.env.DB_HOST
 mongoose.connect('mongodb://172.26.6.6/salmos', { useMongoClient: true } );
 
 mongoose.Promise = global.Promise;
